68456d8b62ed9e43c271a1c1f696acf20a203445
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-02-20 22:43:43 +0500

    py

--- python/divide.py
+++ python/divide.py
@@ -11,6 +11,8 @@ def long_division(dividend, divider):
         print(dividend, "|", 0, sep="")
         return
 
+    last_printed_pos = 0
+    zeros = 0
     idents_count = 0
     position = 0
     result = dividend // divider
@@ -21,29 +23,36 @@ def long_division(dividend, divider):
     lower_n = get_next_lower_n(0, str_result, divider)
     position += int_len(lower_n)
     upper_n = int(str_dividend[:position])
-    idents_count += int_len(upper_n) - int_len(upper_n - lower_n)
     if divider == 1:
         idents_count += 1
     print(dividend, "|", divider, sep="")
     print(lower_n, " " * (int_len(dividend) - int_len(lower_n)), "|", str_result, sep="")
+    last_printed_len = int_len(lower_n)
 
     for i in range(len(str_result) - 1):
+        idents_count += int_len(upper_n) - int_len(upper_n - lower_n)
         upper_n = get_next_upper_n(position, upper_n, lower_n, str_dividend)
-        if upper_n != 0:
-            position += 1
-            print(" " * idents_count, upper_n, sep="")
-
         lower_n = get_next_lower_n(i + 1, str_result, divider)
-        if lower_n != 0:
-            print(" " * idents_count, lower_n, sep="")
-            idents_count += int_len(upper_n) - int_len(upper_n - lower_n)
+        position += 1
+        if upper_n != 0 and lower_n != 0:
+            if (i != len(str_result) - 2):
+                idents_count -= zeros
+                zeros = 0
+            print(" " * idents_count, upper_n, sep="")
+            print(" " * (idents_count + int_len(upper_n) - int_len(lower_n)), lower_n, sep="")
+            last_printed_pos = idents_count + int_len(upper_n) - int_len(lower_n)
+            last_printed_len = int_len(lower_n)
             if divider == 1:
                 idents_count += 1
-
-        elif str_dividend[i] != 0:
+        elif lower_n == 0:
+            zeros += 1
             idents_count += 1
+
+    idents_count += int_len(upper_n) - int_len(upper_n - lower_n)
     if rest != 0:
         print(" " * idents_count, rest, sep="")
+    else:
+        print(" " * (last_printed_pos + last_printed_len - 1), rest, sep="")
 
 
 def int_len(a):
@@ -59,11 +68,11 @@ def get_next_upper_n(position, prev_upper_n, prev_lower_n, str_dividend):
 
 
 def main():
-    print(long_division(1000025, 25))
+    print(long_division(123, 123))
     print()
     print(long_division(1, 1))
     print()
-    print(long_division(12345, 25))
+    print(long_division(15, 3))
     print()
     print(long_division(3, 15))
     print()
@@ -81,7 +90,9 @@ def main():
     print()
     print(long_division(100000, 50))
     print()
-    print(long_division(123456789, 531))
+    print(long_division(472319, 46))
+    print()
+    print(long_division(425934261694251, 12345678))
     print()
     print(long_division(1000025, 25))
 
