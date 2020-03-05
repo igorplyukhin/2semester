b7b169e6caa49ecb0c158c14ad52e4c28a1bdbbe
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-05 22:22:07 +0500

    fuckbrain

--- csharp/6_brainfuck/BrainfuckBasicCommands.cs
+++ csharp/6_brainfuck/BrainfuckBasicCommands.cs
@@ -15,12 +15,18 @@ namespace func.brainfuck
 
         public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
         {
-            vm.RegisterCommand('.', b => write((char) vm.Memory[vm.MemoryPointer]));
-            vm.RegisterCommand('+', b => vm.Memory[vm.MemoryPointer]++);
-            vm.RegisterCommand('-', b => vm.Memory[vm.MemoryPointer]--);
-            vm.RegisterCommand(',', b => vm.Memory[vm.MemoryPointer] = (byte) read());
-            vm.RegisterCommand('>', b => vm.MemoryPointer++);
-            vm.RegisterCommand('<', b => vm.MemoryPointer--);
+            vm.RegisterCommand('.', b =>
+                write((char) vm.Memory[vm.MemoryPointer]));
+            vm.RegisterCommand('+', b =>
+                vm.Memory[vm.MemoryPointer] = (byte) ((vm.Memory[vm.MemoryPointer] + 1) % 256));
+            vm.RegisterCommand('-', b =>
+                vm.Memory[vm.MemoryPointer] = (byte) ((256 + vm.Memory[vm.MemoryPointer] - 1) % 256));
+            vm.RegisterCommand(',', b =>
+                vm.Memory[vm.MemoryPointer] = (byte) read());
+            vm.RegisterCommand('>', b =>
+                vm.MemoryPointer = (vm.MemoryPointer + 1) % vm.Memory.Length);
+            vm.RegisterCommand('<', b =>
+                vm.MemoryPointer = (vm.Memory.Length + vm.MemoryPointer - 1) % vm.Memory.Length);
             foreach (var ch in constants.SelectMany(e => e.Value))
                 vm.RegisterCommand(ch, b => vm.Memory[vm.MemoryPointer] = (byte) ch);
         }
--- csharp/6_brainfuck/BrainfuckLoopCommands.cs
+++ csharp/6_brainfuck/BrainfuckLoopCommands.cs
@@ -2,12 +2,42 @@ using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
-	}
+    public class BrainfuckLoopCommands
+    {
+        private static Dictionary<int, int> brackets;
+        private static Stack<int> openBrackets;
+
+        private static void FindBrackets(IVirtualMachine vm)
+        {
+            brackets = new Dictionary<int, int>();
+            openBrackets = new Stack<int>();
+            for (var i = 0; i < vm.Instructions.Length; i++)
+                switch (vm.Instructions[i])
+                {
+                    case '[':
+                        openBrackets.Push(i);
+                        break;
+                    case ']':
+                        brackets.Add(openBrackets.Peek(), i);
+                        brackets.Add(i, openBrackets.Pop());
+                        break;
+                }
+        }
+        
+
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            FindBrackets(vm);
+            vm.RegisterCommand('[', b =>
+            {
+                if (vm.Memory[vm.MemoryPointer] == 0)
+                    vm.InstructionPointer = brackets[vm.InstructionPointer];
+            });
+            vm.RegisterCommand(']', b =>
+            {
+                if (vm.Memory[vm.MemoryPointer] != 0)
+                    vm.InstructionPointer = brackets[vm.InstructionPointer];
+            });
+        }
+    }
 }
