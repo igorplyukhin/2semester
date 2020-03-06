ef518033070938290547a862d68cbfa9dc7cf986
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-06 17:27:11 +0500

    py

--- csharp/3_smooth/ExpSmoothingTask.cs
+++ csharp/3_smooth/ExpSmoothingTask.cs
@@ -2,24 +2,19 @@ using System.Collections.Generic;
 
 namespace yield
 {
-	public static class ExpSmoothingTask
-	{
-		public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-		{
-			var prevSmoothedY = 0.0;
-			var isFirstIteration = true;
-			foreach (var point in data)
-			{
-				if (isFirstIteration)
-				{
-					prevSmoothedY = point.OriginalY;
-					isFirstIteration = false;
-				}
-
-				point.ExpSmoothedY = prevSmoothedY + alpha * (point.OriginalY - prevSmoothedY);
-				yield return point;
-				prevSmoothedY = point.ExpSmoothedY;
-			}
-		}
-	}
+    public static class ExpSmoothingTask
+    {
+        public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+        {
+            var prevSmoothedY = double.NaN;
+            foreach (var point in data)
+            {
+                prevSmoothedY = double.IsNaN(prevSmoothedY)
+                    ? point.OriginalY
+                    : prevSmoothedY + alpha * (point.OriginalY - prevSmoothedY);
+                point.ExpSmoothedY = prevSmoothedY;
+                yield return point;
+            }
+        }
+    }
 }
