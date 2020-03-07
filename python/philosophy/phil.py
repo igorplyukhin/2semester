d3c180393a8b0974909b306cad0cd5ed8154a834
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-07 14:43:15 +0500

    py

--- python/philosophy/phil.py
+++ python/philosophy/phil.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
+from collections import namedtuple
 import re
 import requests
+import sys
 
 
-def get_content(name):
+def format_title(title):
+    return title.split('|')[0].replace(' ', '_')
+
+
+def get_content(title):
     """
     Функция возвращает содержимое вики-страницы name из русской Википедии.
     В случае ошибки загрузки или отсутствия страницы возвращается None.
     """
-    f = requests.get("https://en.wikipedia.org/w/"
-                     "api.php?action=parse&page={}&prop=wikitext&formatversion=2&format=json".format(name)).json()
+    try:
+        f = requests.get("https://ru.wikipedia.org/w/"
+                     "api.php?action=parse&page={}&prop=wikitext&formatversion=2&format=json".format(title)).json()
+    except:
+        f = "error"
+        pass
+
     if "error" in f:
         return None
     else:
-        return f["parse"]["wikitext"]
+        Page = namedtuple("Page", "title content")
+        return Page(title=format_title(f["parse"]["title"]), content=f["parse"]["wikitext"])
 
 
 def extract_content(page):
@@ -27,13 +39,13 @@ def extract_content(page):
     return (0, len(page))
 
 
-def extract_links(page, begin, end):
+def extract_links(page):
     """
     Функция принимает на вход содержимое страницы и начало и конец интервала,
     задающего позицию содержимого статьи на странице и возвращает все имеющиеся
     ссылки на другие вики-страницы без повторений и с учётом регистра.
     """
-    return re.findall(r"\[\[(.*?)\]\]", page)
+    return [format_title(x) for x in re.findall(r"\[\[(.*?)\]\]", page)]
 
 
 def find_chain(start, finish):
@@ -43,12 +55,29 @@ def find_chain(start, finish):
     Первым элементом результата должен быть start, последним — finish.
     Если построить переходы невозможно, возвращается None.
     """
-    pass
+    current_title = start
+    chain = [current_title]
+    links = [current_title]
+    while current_title != finish:
+        j = None
+        if "Философия" in links:
+            j = get_content("Философия")
+        else:
+            for link in links:
+                j = get_content(link)
+                if j is not None and j.title not in chain and "Файл" not in j.title:
+                    break
+        current_title = j.title
+        links = extract_links(j.content)
+        chain.append(current_title)
+        print(current_title)
 
 
 def main():
-    pass
+    find_chain(sys.argv[1], "Философия")
+    #print(extract_links(get_content("Компьютерная_программа").content))
 
 
 if __name__ == '__main__':
-    print(extract_links(get_content("Sliding_glass_door"), 0, 0))
+    main()
+
