1840489285aba1a07c2612eb3cf540887b7620b4
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-11 12:05:54 +0500

    doodle

---
+++ csharp/7_doodle/ForcesTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace func_rocket
+{
+	public class ForcesTask
+	{
+		public static RocketForce GetThrustForce(double forceValue) =>
+			r => new Vector(1,0).Rotate(r.Direction) * Math.Abs(forceValue);
+
+		public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize) =>
+			r => gravity(spaceSize, r.Location);
+		
+		public static RocketForce Sum(params RocketForce[] forces) => 
+			r => forces.Aggregate(Vector.Zero, (current, e) => current + e(r));
+	}
+}
---
+++ csharp/7_doodle/LevelsTask.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace func_rocket
+{
+	public class LevelsTask
+	{
+		private static Vector initialPos = new Vector(200, 500);
+		static readonly Physics standardPhysics = new Physics();
+		private static Rocket rocket = 
+			new Rocket( initialPos, Vector.Zero, -0.5 * Math.PI);
+		private static Vector target = new Vector(600, 200);
+
+		public static IEnumerable<Level> CreateLevels()
+		{
+			yield return new Level("Zero", 
+				rocket,
+				target, 
+				(size, l) => Vector.Zero, 
+				standardPhysics);
+			
+			yield return new Level("Heavy", 
+				rocket, 
+				target, 
+				(size, l) => new Vector(0, 1) * 0.9,
+				standardPhysics);
+			
+			yield return new Level("Up", 
+				rocket, 
+				new Vector(700, 500), 
+				(size, l) => new Vector(0, -1)*(300 / (size.Height - l.Y + 300.0)),
+				standardPhysics);
+
+			yield return new Level("WhiteHole", 
+				rocket, 
+				target, 
+				CalcWhiteHoleGravity,
+				standardPhysics);
+			
+			yield return new Level("BlackHole", 
+				rocket, 
+				target, 
+				CalcBlackHoleGravity,
+				standardPhysics);	
+			
+			yield return new Level("BlackAndWhite", 
+				rocket, 
+				target,
+				(size, l) => (CalcBlackHoleGravity(size,l) + CalcWhiteHoleGravity(size,l)) / 2,
+				standardPhysics);
+		}
+
+		private static Vector CalcWhiteHoleGravity(Size size, Vector l) 
+		{
+			var d = l - target;
+			return d.Normalize() * 140 * d.Length / (d.Length * d.Length + 1);
+		}
+		
+		private static Vector CalcBlackHoleGravity(Size size, Vector l)
+		{
+			var blackHolePos = (target + initialPos) / 2;
+			var d = blackHolePos - l;
+			return d.Normalize() * 300 * d.Length / (d.Length * d.Length + 1);
+		}
+	}
+}
