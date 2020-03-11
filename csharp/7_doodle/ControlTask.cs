1c4ba15202e985f438e13418006c422048fcc886
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-11 18:30:42 +0500

    ulearn

--- csharp/7_doodle/ControlTask.cs
+++ csharp/7_doodle/ControlTask.cs
@@ -4,9 +4,20 @@ namespace func_rocket
 {
 	public class ControlTask
 	{
+		private const double AngleDelta = 0.00001;
+		private const double SpeedLimit = 2.25;
+
+		private static bool IsTooFast(Rocket rocket) => Math.Abs(rocket.Velocity.Angle - rocket.Direction) > SpeedLimit;
+		
 		public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
+			var currentAngle = (rocket.Velocity.Angle + rocket.Direction) / 2;
+			var appropriateAngle = (target - rocket.Location).Angle;
+			if (Math.Abs(currentAngle - appropriateAngle) < AngleDelta)
+				return Turn.None;
+			if (IsTooFast(rocket))
+				return rocket.Direction < appropriateAngle ? Turn.Right : Turn.Left;
+			return currentAngle < appropriateAngle ? Turn.Right : Turn.Left;
 		}
 	}
 }
