6feb882f371332d22f3f3d4edb7e20e2837585ec
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-03-17 12:49:17 +0500

    refactor1

--- csharp/6_brainfuck/BrainfuckLoopCommands.cs
+++ csharp/6_brainfuck/BrainfuckLoopCommands.cs
@@ -4,30 +4,9 @@ namespace func.brainfuck
 {
     public class BrainfuckLoopCommands
     {
-        private static Dictionary<int, int> brackets;
-        private static Stack<int> openBrackets;
-
-        private static void FindBrackets(IVirtualMachine vm)
-        {
-            brackets = new Dictionary<int, int>();
-            openBrackets = new Stack<int>();
-            for (var i = 0; i < vm.Instructions.Length; i++)
-                switch (vm.Instructions[i])
-                {
-                    case '[':
-                        openBrackets.Push(i);
-                        break;
-                    case ']':
-                        brackets.Add(openBrackets.Peek(), i);
-                        brackets.Add(i, openBrackets.Pop());
-                        break;
-                }
-        }
-        
-
         public static void RegisterTo(IVirtualMachine vm)
         {
-            FindBrackets(vm);
+            var brackets = FindBrackets(vm.Instructions);
             vm.RegisterCommand('[', b =>
             {
                 if (vm.Memory[vm.MemoryPointer] == 0)
@@ -38,6 +17,25 @@ namespace func.brainfuck
                 if (vm.Memory[vm.MemoryPointer] != 0)
                     vm.InstructionPointer = brackets[vm.InstructionPointer];
             });
+        }
+
+        private static Dictionary<int, int> FindBrackets(string instructions)
+        {
+            var brackets = new Dictionary<int, int>();
+            var openBrackets = new Stack<int>();
+            for (var i = 0; i < instructions.Length; i++)
+                switch (instructions[i])
+                {
+                    case '[':
+                        openBrackets.Push(i);
+                        break;
+                    case ']':
+                        brackets.Add(openBrackets.Peek(), i);
+                        brackets.Add(i, openBrackets.Pop());
+                        break;
+                }
+
+            return brackets;
         }
     }
 }
--- csharp/7_doodle/LevelsTask.cs
+++ csharp/7_doodle/LevelsTask.cs
@@ -6,62 +6,47 @@ namespace func_rocket
 {
     public class LevelsTask
     {
-        private static Vector initialPos = new Vector(200, 500);
-        static readonly Physics standardPhysics = new Physics();
+        private static readonly Vector initialPos = new Vector(200, 500);
+        private static readonly Physics standardPhysics = new Physics();
 
-        private static Rocket rocket =
+        private static readonly Rocket defultRocket =
             new Rocket(initialPos, Vector.Zero, -0.5 * Math.PI);
 
-        private static Vector target = new Vector(600, 200);
+        private static readonly Vector defaultTarget = new Vector(600, 200);
 
-        public class DefaultLevel : Level
+        public static IEnumerable<Level> CreateLevels() => levels;
+
+        private static readonly List<Level> levels = new List<Level>
         {
-            public DefaultLevel(string name = null, Rocket rocket = null, Vector target = null,
-                Gravity gravity = null, Physics physics = null) : 
-                base("no name", LevelsTask.rocket, LevelsTask.target, (size, l) => Vector.Zero, standardPhysics)
-            {
-                if (name != null)
-                    Level.Name = name;
-                if (rocket != null)
-                    rocket = LevelsTask.rocket;
-                if (target != null)
-                    target = LevelsTask.target;
-                if (gravity != null)
-                    gravity = (size, l) => Vector.Zero;
-                if (physics != null)
-                    physics = standardPhysics;
-            } 
+            InitLevel("Zero"),
+            InitLevel("Heavy", gravity: (size, l) => new Vector(0, 1) * 0.9),
+            InitLevel("Up", target: new Vector(700, 500),
+                gravity: (size, l) => new Vector(0, -1) * (300 / (size.Height - l.Y + 300.0))),
+            InitLevel("WhiteHole", gravity: CalcWhiteHoleGravity),
+            InitLevel("BlackHole", gravity: CalcBlackHoleGravity),
+            InitLevel("BlackAndWhite",
+                gravity: (size, l) => (CalcBlackHoleGravity(size, l) + CalcWhiteHoleGravity(size, l)) / 2)
+        };
+
+        private static Level InitLevel(string name = "NoName", Rocket rocket = null, Vector target = null,
+            Gravity gravity = null, Physics physics = null)
+        {
+            rocket = rocket ?? defultRocket;
+            target = target ?? defaultTarget;
+            gravity = gravity ?? ((size, l) => Vector.Zero);
+            physics = physics ?? standardPhysics;
+            return new Level(name, rocket, target, gravity, physics);
         }
-
-        private static List<Level> levels = new List<Level>
-        {
-            new Level("Zero", rocket, target, (size, l) => Vector.Zero, standardPhysics),
-            new Level("Heavy", rocket, target,
-                (size, l) => new Vector(0, 1) * 0.9, standardPhysics),
-            new Level("Up", rocket, new Vector(700, 500),
-                (size, l) => new Vector(0, -1) * (300 / (size.Height - l.Y + 300.0)), standardPhysics),
-            new Level("WhiteHole", rocket, target, CalcWhiteHoleGravity, standardPhysics),
-            new Level("BlackHole", rocket, target, CalcBlackHoleGravity, standardPhysics),
-            new Level("BlackAndWhite", rocket, target,
-                (size, l) => (CalcBlackHoleGravity(size, l) + CalcWhiteHoleGravity(size, l)) / 2,
-                standardPhysics)
-        };
-
-        public static IEnumerable<Level> CreateLevels()
-        {
-            
-        };
 
         private static Vector CalcWhiteHoleGravity(Size size, Vector l)
         {
-            var d = l - target;
+            var d = l - defaultTarget;
             return d.Normalize() * 140 * d.Length / (d.Length * d.Length + 1);
         }
 
         private static Vector CalcBlackHoleGravity(Size size, Vector l)
         {
-            var blackHolePos = (target + initialPos) / 2;
-            var d = blackHolePos - l;
+            var d = (defaultTarget + initialPos) / 2 - l;
             return d.Normalize() * 300 * d.Length / (d.Length * d.Length + 1);
         }
     }
