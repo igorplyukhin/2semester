bbb30a0bbb5e6d0b647fe3b2512bd66ac09c982b
Igor <51264207+igorplyukhin@users.noreply.github.com>
2020-04-01 12:30:46 +0500

    dungeon

--- csharp/9_dungeon/BfsTask.cs
+++ csharp/9_dungeon/BfsTask.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
 
 namespace Dungeon
 {
@@ -10,51 +8,40 @@ namespace Dungeon
     {
         public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
-            var a = FindShortestPath(map, start, map.InitialPosition);
-                if (a != null)
-                    yield return a;
-        }
-
-        private static SinglyLinkedList<Point> FindShortestPath(Map map, Point start, Point finish)
-        {
-            var track = new Dictionary<Point, Point> {[start] = new Point(int.MinValue, int.MinValue)};
-            var queue = new Queue<Point>();
-            queue.Enqueue(start);
+            var visitedPoints = new HashSet<Point>();
+            var hashedChests = new HashSet<Point>(chests);
+            var queue = new Queue<SinglyLinkedList<Point>>();
+            queue.Enqueue(new SinglyLinkedList<Point>(start));
             var width = map.Dungeon.GetLength(0);
             var height = map.Dungeon.GetLength(1);
 
             while (queue.Count != 0)
             {
                 var point = queue.Dequeue();
-                
-
                 for (var dy = -1; dy <= 1; dy++)
                 for (var dx = -1; dx <= 1; dx++)
                 {
-                    var nextPoint = new Point(point.X + dx, point.Y + dy);
-                    if (dx != 0 && dy != 0 || track.ContainsKey(nextPoint)) continue;
-                    if (nextPoint.X < 0 || nextPoint.X >= width || nextPoint.Y < 0 || nextPoint.Y >= height) continue;
-                    if (map.Dungeon[nextPoint.X, nextPoint.Y] != MapCell.Empty) continue;
-                    track.Add(nextPoint, point);
-                    queue.Enqueue(nextPoint);
+                    var nextPoint = new Point(point.Value.X + dx, point.Value.Y + dy);
+                    if (IsWrongMove(point.Value, nextPoint, width, height, visitedPoints, map)) continue;
+                    visitedPoints.Add(nextPoint);
+                    var nextNode = new SinglyLinkedList<Point>(nextPoint, point);
+                    if (hashedChests.Contains(nextPoint))
+                        yield return nextNode;
+                    queue.Enqueue(nextNode);
                 }
-
-                if (track.ContainsKey(finish)) break;
             }
+        }
 
-            if (!track.ContainsKey(finish))
-                return null;
-            var item = finish;
-            var l = new SinglyLinkedList<Point>(item);
-            item = track[item];
-
-            while (item.X != int.MinValue && item.Y != int.MinValue)
-            {
-                l = new SinglyLinkedList<Point>(item, l);
-                item = track[item];
-            }
-            
-            return l;
+        private static bool IsWrongMove(Point oldP, Point newP, int width, int height, 
+            HashSet<Point> visitedPoints, Map map)
+        {
+            return Math.Abs(oldP.X - newP.X) != 0 && Math.Abs(oldP.Y - newP.Y) != 0
+                   || visitedPoints.Contains(newP)
+                   || newP.X < 0 
+                   || newP.X >= width 
+                   || newP.Y < 0 
+                   || newP.Y >= height 
+                   || map.Dungeon[newP.X, newP.Y] != MapCell.Empty;
         }
     }
 }
--- csharp/9_dungeon/DungeonTask.cs
+++ csharp/9_dungeon/DungeonTask.cs
@@ -5,14 +5,60 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Dungeon
 {
-	public class DungeonTask
-	{
-		public static MoveDirection[] FindShortestPath(Map map)
-		{
-			return new MoveDirection[0];
-		}
-	}
+    public class DungeonTask
+    {
+        public static MoveDirection[] FindShortestPath(Map map)
+        {
+            var straightWay = GetStraightWay(map);
+            var straightDirections = ConvertPointsToDirections(straightWay).ToArray();
+            foreach (var point in straightWay)
+            {
+                if (map.Chests.Contains(point))
+                    return straightDirections;
+            }
+            
+            var pathsToPlayer = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
+            if (!pathsToPlayer.Any())
+                return straightDirections;
+            var pathsToExit = BfsTask.FindPaths(map, map.Exit, map.Chests);
+            var fullPaths = pathsToPlayer.Join(pathsToExit, list => list.Value, list => list.Value,
+                (l1, l2) => l1
+                    .Reverse()
+                    .Concat(l2.Skip(1))
+                    .ToList());
+            if (!fullPaths.Any())
+                return new MoveDirection[0];
+            
+            var shortestPath = fullPaths.Aggregate((min, x) => x.Count < min.Count ? x : min);
+            
+            return ConvertPointsToDirections(shortestPath).ToArray();
+        }
+
+        private static IEnumerable<MoveDirection> ConvertPointsToDirections(IEnumerable<Point> points)
+        {
+            return points.Zip(points.Skip(1), (p1, p2) =>
+            {
+                var dx = p2.X - p1.X;
+                var dy = p2.Y - p1.Y;
+                if (dx < 0)
+                    return MoveDirection.Left;
+                if (dx > 0)
+                    return MoveDirection.Right;
+                if (dy < 0)
+                    return MoveDirection.Up;
+                return MoveDirection.Down;
+            }).ToArray();
+        }
+
+        private static IEnumerable<Point> GetStraightWay(Map map)
+        {
+            var path = 
+                BfsTask.FindPaths(map, map.InitialPosition, new[] {new Point(map.Exit.X, map.Exit.Y)});
+            return path;
+        }
+    }
 }
